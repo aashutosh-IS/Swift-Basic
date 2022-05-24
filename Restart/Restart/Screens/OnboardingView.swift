f910cdf1b26c2fc75296059345aeeffefb22888b
Aashutosh <>
2022-05-24 08:58:37 +0545

    Added designs in on boarding for Restart-App

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -14,16 +14,106 @@ struct OnboardingView: View {
     
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding").font(.largeTitle)
-            Button {
-                //Some actions
-                isOnboardingViewActive = false
-            } label: {
-                Text("Start")
-            }
-
-        } // VSTACK - Like Column
+        ZStack {
+            Color("ColorBlue").ignoresSafeArea(.all,edges: .all)
+            
+            VStack(spacing: 20) {
+             // header
+                Spacer()
+                
+                VStack(spacing : 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                
+Text("""
+                        It's not how much we give but
+                        how much love we put into giving.
+                        """).font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal,10)
+                }
+            // center
+                
+                ZStack{
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2),lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2),lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                    
+                }
+                
+                Spacer()
+            //footer
+                
+                ZStack{
+                    //parts of button
+                    
+                    //1. Backgrounng
+                    
+                        Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    //2. call to action
+                    
+                    Text("Get Started")
+                        .font(.system(.title3,design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    
+                    //3. capsule shape
+                    
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                        
+                    }
+                    //4. dragabble circle
+                    
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24,weight: .bold))
+                        }.foregroundColor(.white)
+                            .frame(width: 80, height: 80, alignment: .center)
+                            .onTapGesture {
+                                isOnboardingViewActive=false
+                            }
+                        Spacer()
+                    }
+                    
+                    
+                    
+                    
+                    
+                }//:Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
+                
+            }// :VSTACK
+        } // ZSTACK - Like Column
     }
 }
 
